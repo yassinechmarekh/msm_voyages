15239380cf76f4b58671305e2680433e22d3f5bd
yassine <yassinechmarekh88@gmail.com>
2024-09-01 01:11:25 +0100

    add offers section

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,25 +1,51 @@
 // populaire swiper
 var swiper = new Swiper(".populaire-swp ", {
-    pagination: {
-      el: ".swiper-pagination",
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
     },
-    loop: true,
-    slidesPerView:3,
-    spaceBetween: 30,
-    autoplay:{
-      delay:2500,
+    775: {
+      slidesPerView: 2,
+      spaceBetween: 15,
     },
-    breakpoints: {
-      1200: {
-        slidesPerView: 3,
-      },
-      775: {
-        slidesPerView: 2,
-        spaceBetween: 15,
-      },
-      0: {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
-  });
+  },
+});
+
+// offre swiper
+var swiper = new Swiper(".offre-swp ", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+    },
+    775: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+});
