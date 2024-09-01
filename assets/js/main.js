63212cf8de978f1a4fe6d6ccaf598200acbe3f40
yassine <yassinechmarekh88@gmail.com>
2024-09-01 02:24:14 +0100

    add loader and scrollbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,36 @@
 // active & enactive modal
-modal = document.querySelector('.modal');
-background_modal = document.querySelector('.background_modal');
+modal = document.querySelector(".modal");
+background_modal = document.querySelector(".background_modal");
 
-function active_modal(){
-    modal.classList.add("active");
-    background_modal.classList.add("active");
+function active_modal() {
+  modal.classList.add("active");
+  background_modal.classList.add("active");
 }
 
-function close_modal(){
-    modal.classList.remove("active");
-    background_modal.classList.remove("active");
+function close_modal() {
+  modal.classList.remove("active");
+  background_modal.classList.remove("active");
 }
-
 
 // modal radio button
 document.addEventListener("DOMContentLoaded", function () {
-    const radioButtons = document.querySelectorAll('input[name="place_number"]');
-    const numberInput = document.getElementById("number_place");
+  const radioButtons = document.querySelectorAll('input[name="place_number"]');
+  const numberInput = document.getElementById("number_place");
 
-    radioButtons.forEach((radioButton) => {
-      radioButton.addEventListener("change", function () {
-        if (document.getElementById("plus").checked) {
-          numberInput.classList.add("active");
-        } else {
-          numberInput.classList.remove("active");
-        }
-      });
+  radioButtons.forEach((radioButton) => {
+    radioButton.addEventListener("change", function () {
+      if (document.getElementById("plus").checked) {
+        numberInput.classList.add("active");
+      } else {
+        numberInput.classList.remove("active");
+      }
     });
   });
+});
+
+// loader
+window.addEventListener("load", function () {
+    document.getElementById("loader-container").style.display = "none";
+    document.body.classList.remove("no-scroll");
+  });
+  document.body.classList.add("no-scroll");
